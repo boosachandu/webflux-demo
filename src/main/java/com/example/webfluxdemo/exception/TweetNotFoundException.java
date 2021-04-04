32bad8c6e6508a395ed6e088e1889cda0b716f93
boosachandu <boosachandu@gmail.com>
2021-04-04 20:53:37 +0530

    Added the new stuff

---
+++ src/main/java/com/example/webfluxdemo/exception/TweetNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.webfluxdemo.exception;
+
+
+public class TweetNotFoundException extends RuntimeException {
+
+    public TweetNotFoundException(String tweetId) {
+        super("Tweet not found with id " + tweetId);
+    }
+}
